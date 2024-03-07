4448e7f8a54a4d293714a01706498501c307e045
Alex <17277800+alex-schaaf@users.noreply.github.com>
2024-03-07 07:02:38 +0100

    add arr utils

--- utils/utils.go
+++ utils/utils.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"strconv"
 )
 
 func ReadLines(filepath string) ([]string, error) {
@@ -19,4 +20,26 @@ func ReadLines(filepath string) ([]string, error) {
 	}
 
 	return lines, scanner.Err()
+}
+
+func ArrMin(arr []int) int {
+	min := arr[0]
+	for _, val := range arr[1:] {
+		if val < min {
+			min = val
+		}
+	}
+	return min
+}
+
+func ArrStrToInt(arrStr []string) []int {
+	var arrInt []int
+	for _, str := range arrStr {
+		num, err := strconv.Atoi(str)
+		if err != nil {
+			log.Fatal(err)
+		}
+		arrInt = append(arrInt, num)
+	}
+	return arrInt
 }
