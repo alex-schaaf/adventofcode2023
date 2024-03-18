c154af4ef36272acea7c1ba1712181938420a5c9
Alex <17277800+alex-schaaf@users.noreply.github.com>
2024-03-18 23:06:59 +0100

    add solution for day 8.2

--- day08/main.py
+++ day08/main.py
@@ -1,4 +1,5 @@
 import re
+import math
 
 with open("input.txt", "r") as file:
     lines = file.read().split("\n")
@@ -39,11 +40,56 @@ def find_path(start: str, end: str, nodes: dict[str, Node], instructions: str):
 
     return steps
 
-start = "AAA"
-end = "ZZZ"
+
+def task1(instructions, nodes) -> int:
+    start = "AAA"
+    end = "ZZZ"
+
+    instructions = lines[0]
+    nodes = parse_nodes(lines[2:-1])
+
+    result = find_path(start, end, nodes, instructions)
+    return result
+
 
 instructions = lines[0]
 nodes = parse_nodes(lines[2:-1])
 
-result = find_path(start, end, nodes, instructions)
-print(result)
+print("Task 1:", task1(instructions, nodes))
+
+
+def task2(instructions: str, nodes: dict[str, Node]) -> int:
+    """
+    Using the same logic as in task1, we can find the number of steps it takes
+    for each start not to reach its respective end node. So for each start node
+    we have this cycle number. We then need to find the lowest common multiple
+    (LCM), as thats the number of steps when all cycles are in sync.
+
+    Example:
+
+    Starting with 2 nodes, A and B, one with cycle 2 and one with cycle 3, the
+    LCM is 6.
+
+       1 2  3  4  5 6
+    A [x x][x  x][x x] (cycle 2)
+    B [x x  x][x  x x] (cycle 3)
+    """
+    start_nodes = [n for n in nodes.values() if n.name.endswith("A")]
+    node_cycles = []
+
+    for current in start_nodes:
+        steps = 0
+        while not current.name.endswith("Z"):
+            instruction = instructions[steps % len(instructions)]
+            if instruction == "L":
+                current = nodes[current.left]
+            else:
+                current = nodes[current.right]
+            steps += 1
+
+        node_cycles.append(steps)
+
+    return math.lcm(*node_cycles)
+
+
+print("Task 2:", task2(instructions, nodes))
